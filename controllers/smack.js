a21a9e28ee5ea602177a37b453426dd85281fd83
Jen Schumann <jenniferschumann1@gmail.com>
2018-01-11 19:57:31 -0700

    added 3rd model controller

---
+++ controllers/smack.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const Smack = require('../models/smack.js');
+
+
+//index route
+router.get('/', function(req, res){
+  Smack.find({}, function(err, foundSmack){
+    res.json(foundSmack);
+  });
+});
+
+//show route
+router.get('/:id', (req, res)=> {
+  Smack.find({_id : req.params.id }, function(err, foundSmack){
+    res.send(foundSmack);
+  });
+});
+
+//create route
+router.post('/', function(req, res){
+  Smack.create(req.body, function(err, createdSmack){
+    res.json(createdSmack);
+  });
+});
+
+//update route
+router.put('/:id', function(req, res){
+  Smack.findByIdAndUpdate(req.params.id, req.body, {new:true}, function(err, updatedSmack){
+    res.json(updatedSmack);
+  });
+});
+
+//delete route
+router.delete('/:id', function(req, res){
+  Smack.findByIdAndRemove(req.params.id, function(err, deletedSmack){
+    res.json(deletedSmack);
+  });
+});
+
+module.exports = router;
--- models/smack.js
+++ models/smack.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const smackSchema = mongoose.Schema({
   smackTitle: String,
   author: { type:String, default:'' },
+  img: String,
   smackdown: String,
   tag: String
 });
