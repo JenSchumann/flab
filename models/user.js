5529ae37a4c0ae6b0025e23b60f295fa37588d7a
Jen Schumann <jenniferschumann1@gmail.com>
2018-01-19 13:42:13 -0700

    new packages added and required in server and user model created

---
+++ models/user.js
@@ -0,0 +1,27 @@
+//dependencies
+const mongoose = require('mongoose');
+const Beer = require('./beers.js');
+const Football = require('./football.js');
+const Smack = require('./smack.js');
+
+
+//Schema
+const userSchema = mongoose.Schema({
+  name: String,
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+
+  image: { type: String, default: '' },
+  bio: { type: String, default: '' },
+
+  beers: [Beer.schema],
+  football: [Football.schema],
+  smack: [Smack.schema]
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+
+//export
+module.exports = User;
